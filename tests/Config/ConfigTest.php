c6a125055214263df5c280641fcc23876fa5adac
Антон Круглов <usualtsun@gmail.com>
2025-03-21 13:14:56 +0300

    test: Config and Logger tests;

--- tests/Config/ConfigTest.php
+++ tests/Config/ConfigTest.php
@@ -12,101 +12,107 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
 
+use function sprintf;
+
 #[CoversClass(Config::class)]
 final class ConfigTest extends TestCase
 {
-	/**
-	 * @param array<array-key, mixed> $extra
-	 * @throws InvalidComposerExtraConfigException
-	 * @throws ExpectationFailedException
-	 */
-	#[Test]
-	#[DataProvider('configProvider')]
-	public function successInstantiatedConfig(array $extra, string $expectedRepository, bool $expectedRepositoryEmpty): void
-	{
-		$config = new Config($extra);
-		
-		self::assertSame($expectedRepository, $config->getRepository());
-		self::assertSame($expectedRepositoryEmpty, $config->repositoryIsEmpty());
-	}
-	
-	/**
-	 * @param array<array-key, mixed> $extra
-	 * @param non-empty-string $message
-	 * @throws InvalidComposerExtraConfigException
-	 */
-	#[Test]
-	#[DataProvider('invalidConfigProvider')]
-	public function cannotBeInstantiatedWithInvalidConfig(array $extra, string $message): void
-	{
-		$this->expectException(InvalidComposerExtraConfigException::class);
-		$this->expectExceptionMessage($message);
-		
-		new Config($extra);
-	}
-	
-	/**
-	 * @return iterable<non-empty-string, array{0: array<array-key, mixed>, 1: string}>
-	 */
-	public static function configProvider(): iterable
-	{
-		yield 'default value' => [
-			[],
-			'',
-			true,
-		];
-		yield 'empty extra key' => [
-			[
-				'unknown' => [],
-			],
-			'',
-			true,
-		];
-		yield 'as default but explicit' => [
-			[
-				Config::EXTRA_CONFIG_KEY => [
-					Config::REPOSITORY => '',
-				],
-			],
-			'',
-			true,
-		];
-		yield 'explicit' => [
-			[
-				Config::EXTRA_CONFIG_KEY => [
-					Config::REPOSITORY => 'https://github.com/neontsun/composer-devtools-plugin',
-				],
-			],
-			'https://github.com/neontsun/composer-devtools-plugin',
-			false,
-		];
-	}
-	
-	/**
-	 * @return iterable<non-empty-string, array{0: array<array-key, mixed>, 1: non-empty-string}>
-	 */
-	public static function invalidConfigProvider(): iterable
-	{
-		yield 'non array extra config key' => [
-			[
-				Config::EXTRA_CONFIG_KEY => 'foo',
-			],
-			sprintf(
-				'Expected setting "extra.%s" to be a array value. Got "string".',
-				Config::EXTRA_CONFIG_KEY,
-			),
-		];
-		yield 'non string repository' => [
-			[
-				Config::EXTRA_CONFIG_KEY => [
-					Config::REPOSITORY => 123,
-				],
-			],
-			sprintf(
-				'Expected setting "extra.%s.%s" to be a string value. Got "int".',
-				Config::EXTRA_CONFIG_KEY,
-				Config::REPOSITORY,
-			),
-		];
-	}
+    /**
+     * @param array<array-key, mixed> $extra
+     * @throws InvalidComposerExtraConfigException
+     * @throws ExpectationFailedException
+     */
+    #[Test]
+    #[DataProvider('configProvider')]
+    public function successInstantiatedConfig(array $extra, string $expectedRepository, bool $expectedRepositoryEmpty): void
+    {
+        $config = new Config($extra);
+
+        self::assertSame($expectedRepository, $config->getRepository());
+        self::assertSame($expectedRepositoryEmpty, $config->repositoryIsEmpty());
+    }
+
+    /**
+     * @param array<array-key, mixed> $extra
+     * @param non-empty-string $message
+     * @throws InvalidComposerExtraConfigException
+     */
+    #[Test]
+    #[DataProvider('invalidConfigProvider')]
+    public function cannotBeInstantiatedWithInvalidConfig(array $extra, string $message): void
+    {
+        $this->expectException(InvalidComposerExtraConfigException::class);
+        $this->expectExceptionMessage($message);
+
+        new Config($extra);
+    }
+
+    /**
+     * @return iterable<non-empty-string, array{0: array<array-key, mixed>, 1: string}>
+     */
+    public static function configProvider(): iterable
+    {
+        yield 'default value' => [
+            [],
+            '',
+            true,
+        ];
+
+        yield 'empty extra key' => [
+            [
+                'unknown' => [],
+            ],
+            '',
+            true,
+        ];
+
+        yield 'as default but explicit' => [
+            [
+                Config::EXTRA_CONFIG_KEY => [
+                    Config::REPOSITORY => '',
+                ],
+            ],
+            '',
+            true,
+        ];
+
+        yield 'explicit' => [
+            [
+                Config::EXTRA_CONFIG_KEY => [
+                    Config::REPOSITORY => 'https://github.com/neontsun/composer-devtools-plugin',
+                ],
+            ],
+            'https://github.com/neontsun/composer-devtools-plugin',
+            false,
+        ];
+    }
+
+    /**
+     * @return iterable<non-empty-string, array{0: array<array-key, mixed>, 1: non-empty-string}>
+     */
+    public static function invalidConfigProvider(): iterable
+    {
+        yield 'non array extra config key' => [
+            [
+                Config::EXTRA_CONFIG_KEY => 'foo',
+            ],
+            sprintf(
+                'Expected setting "extra.%s" to be a array value. Got "string".',
+                Config::EXTRA_CONFIG_KEY,
+            ),
+        ];
+
+        yield 'non string repository' => [
+            [
+                Config::EXTRA_CONFIG_KEY => [
+                    Config::REPOSITORY => 123,
+                ],
+            ],
+            sprintf(
+                'Expected setting "extra.%s.%s" to be a string value. Got "int".',
+                Config::EXTRA_CONFIG_KEY,
+                Config::REPOSITORY,
+            ),
+        ];
+    }
 }
--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace Neontsun\Composer\Devtools\Tests;
 
+use const PHP_EOL;
+
 use Composer\IO\BufferIO;
 use Neontsun\Composer\Devtools\Logger;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -17,96 +19,98 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[CoversClass(Logger::class)]
 final class LoggerTest extends TestCase
 {
-	private const VERBOSITIES = [
-		OutputInterface::VERBOSITY_QUIET,
-		OutputInterface::VERBOSITY_NORMAL,
-		OutputInterface::VERBOSITY_VERBOSE,
-		OutputInterface::VERBOSITY_VERY_VERBOSE,
-		OutputInterface::VERBOSITY_DEBUG,
-	];
-	
-	/**
-	 * @throws ExpectationFailedException
-	 * @throws RuntimeException
-	 */
-	#[Test]
-	#[DataProvider('messageProvider')]
-	public function successLogMessage(int $verbosity, string $message, string $expected): void
-	{
-		$io = new BufferIO(input: '', verbosity: $verbosity);
-		$logger = new Logger($io);
-		
-		$logger->info($message);
-		
-		self::assertSame($expected, $io->getOutput());
-	}
-	
-	/**
-	 * @throws ExpectationFailedException
-	 * @throws RuntimeException
-	 */
-	#[Test]
-	#[DataProvider('messageDebugProvider')]
-	public function successLogDebugMessage(int $verbosity, string $message, string $expected): void
-	{
-		$io = new BufferIO(input: '', verbosity: $verbosity);
-		$logger = new Logger($io);
-		
-		$logger->debug($message);
-		
-		self::assertSame($expected, $io->getOutput());
-	}
-	
-	/**
-	 * @return iterable<array{0: positive-int, 1: non-empty-string, 2: non-empty-string}>
-	 */
-	public static function messageProvider(): iterable
-	{
-		$notLoggedVerbosities = [
-			OutputInterface::VERBOSITY_QUIET,
-		];
-		
-		$loggedVerbosities = array_diff(
-			self::VERBOSITIES,
-			$notLoggedVerbosities
-		);
-		
-		$message = 'Hello, Foo!';
-		$expected = '[devtools] Hello, Foo!' . PHP_EOL;
-		
-		foreach ($notLoggedVerbosities as $verbosity) {
-			yield [$verbosity, $message, ''];
-		}
-		
-		foreach ($loggedVerbosities as $verbosity) {
-			yield [$verbosity, $message, $expected];
-		}
-	}
-	
-	/**
-	 * @return iterable<array{0: positive-int, 1: non-empty-string, 2: non-empty-string}>
-	 */
-	public static function messageDebugProvider(): iterable
-	{
-		$notLoggedVerbosities = [
-			OutputInterface::VERBOSITY_QUIET,
-			OutputInterface::VERBOSITY_NORMAL,
-		];
-		
-		$loggedVerbosities = array_diff(
-			self::VERBOSITIES,
-			$notLoggedVerbosities
-		);
-		
-		$message = 'Hello, Foo!';
-		$expected = '[devtools] Hello, Foo!' . PHP_EOL;
-		
-		foreach ($notLoggedVerbosities as $verbosity) {
-			yield [$verbosity, $message, ''];
-		}
-		
-		foreach ($loggedVerbosities as $verbosity) {
-			yield [$verbosity, $message, $expected];
-		}
-	}
+    private const VERBOSITIES = [
+        OutputInterface::VERBOSITY_QUIET,
+        OutputInterface::VERBOSITY_NORMAL,
+        OutputInterface::VERBOSITY_VERBOSE,
+        OutputInterface::VERBOSITY_VERY_VERBOSE,
+        OutputInterface::VERBOSITY_DEBUG,
+    ];
+
+    /**
+     * @param non-empty-string $message
+     * @throws ExpectationFailedException
+     * @throws RuntimeException
+     */
+    #[Test]
+    #[DataProvider('messageProvider')]
+    public function successLogMessage(int $verbosity, string $message, string $expected): void
+    {
+        $bufferIO = new BufferIO(input: '', verbosity: $verbosity);
+        $logger = new Logger($bufferIO);
+
+        $logger->info($message);
+
+        self::assertSame($expected, $bufferIO->getOutput());
+    }
+
+    /**
+     * @param non-empty-string $message
+     * @throws ExpectationFailedException
+     * @throws RuntimeException
+     */
+    #[Test]
+    #[DataProvider('messageDebugProvider')]
+    public function successLogDebugMessage(int $verbosity, string $message, string $expected): void
+    {
+        $bufferIO = new BufferIO(input: '', verbosity: $verbosity);
+        $logger = new Logger($bufferIO);
+
+        $logger->debug($message);
+
+        self::assertSame($expected, $bufferIO->getOutput());
+    }
+
+    /**
+     * @return iterable<array{0: positive-int, 1: non-empty-string, 2: string}>
+     */
+    public static function messageProvider(): iterable
+    {
+        $notLoggedVerbosities = [
+            OutputInterface::VERBOSITY_QUIET,
+        ];
+
+        $loggedVerbosities = array_diff(
+            self::VERBOSITIES,
+            $notLoggedVerbosities,
+        );
+
+        $message = 'Hello, Foo!';
+        $expected = '[devtools] Hello, Foo!' . PHP_EOL;
+
+        foreach ($notLoggedVerbosities as $verbosity) {
+            yield [$verbosity, $message, ''];
+        }
+
+        foreach ($loggedVerbosities as $verbosity) {
+            yield [$verbosity, $message, $expected];
+        }
+    }
+
+    /**
+     * @return iterable<array{0: positive-int, 1: non-empty-string, 2: string}>
+     */
+    public static function messageDebugProvider(): iterable
+    {
+        $notLoggedVerbosities = [
+            OutputInterface::VERBOSITY_QUIET,
+            OutputInterface::VERBOSITY_NORMAL,
+        ];
+
+        $loggedVerbosities = array_diff(
+            self::VERBOSITIES,
+            $notLoggedVerbosities,
+        );
+
+        $message = 'Hello, Foo!';
+        $expected = '[devtools] Hello, Foo!' . PHP_EOL;
+
+        foreach ($notLoggedVerbosities as $verbosity) {
+            yield [$verbosity, $message, ''];
+        }
+
+        foreach ($loggedVerbosities as $verbosity) {
+            yield [$verbosity, $message, $expected];
+        }
+    }
 }
